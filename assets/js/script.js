1fcd1166e6bac2cf09e13af99478951646e912f5
rdylanward <r.dylan.ward@gmail.com>
2021-03-25 20:03:09 +0000

    Created a mouse event that is not working. Will revisit again.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
 /**********************************************************************/
+/***                         RIPPLE EFFECT                          ***/
+/**********************************-***********************************/
+
+const btnOrange = document.getElementById('btn-orange');
+
+let ripple;
+
+btnOrange.addEventListener("mouseenter", (e) => {
+    const left = e.clientX - e.target.getBoundingClientRect().left;
+    const top = e.clientY - e.target.getBoundingClientRect().top;
+
+    ripple.document.createElement("div");
+    ripple.classList.add("ripple");
+    ripple.style.left = `${left}px`;
+    ripple.style.top = `${top}px`;
+    btnOrange.prepend(ripple);
+})
+
+btnOrange.addEventListener("mouseleave", () => {
+    btnOrange.removeChild(ripple);
+})
+
+
+/**********************************************************************/
 /***                     Initialise Parameters                      ***/
 /**********************************-***********************************/
 
-
 window.onscroll = function() {stickyNavbar()};
 
-let navbar = document.getElementById("navbar");
+const navbar = document.getElementById("navbar");
 let navbarMobile = document.getElementById("navbar-mobile");
 let sticky = navbar.offsetHeight;
 
@@ -14,14 +37,10 @@ let modalEmailContainer = document.getElementById("modal-email-container");
 let modalEmailBox = document.getElementById("modal-email-box");
 let modalEmailBtn = document.getElementById("modal-email-btn");
 
-const btn = document.querySelector(".btn");
-let ripple;
-
 
 /**********************************************************************/
 /***                         STICKY NAVBAR                          ***/
 /**********************************-***********************************/
-
 
 /* The following Javascript was originally written by W3Schools (https://www.w3schools.com/howto/howto_js_navbar_sticky.asp)
    and repurposed to aid in the function of the sticky menu */
@@ -56,7 +75,6 @@ function mobileMenu() {
 /***                        EMAIL MODAL BOX                         ***/
 /**********************************-***********************************/
 
-
 /* The following Javascript was originally written by W3Schools (https://www.w3schools.com/howto/howto_css_modals.asp)
    and repurposed to aid in the function of the email modal box. When the user clicks on the button, it opens the modal */
 modalEmailSubmit.onclick = function() {
@@ -69,23 +87,3 @@ modalEmailBtn.onclick = function() {
     modalEmailContainer.style.display = "none";
     modalEmailBox.style.display = "none";
 }
-
-
-/**********************************************************************/
-/***                         RIPPLE EFFECT                          ***/
-/**********************************-***********************************/
-
-btn.addEventListener("mouseenter", (e) => {
-    const left = e.clientX - e.target.getBoundingClientRect().left;
-    const top = e.clientY - e.target.getBoundingClientRect().top;
-
-    ripple.document.createElement("div");
-    ripple.classList.add("ripple");
-    ripple.style.left = `${left}px`;
-    ripple.style.top = `${top}px`;
-    btn.prepend(ripple);
-})
-
-btn.addEventListener("mouseleave", () => {
-    btn.removeChild(ripple);
-})
