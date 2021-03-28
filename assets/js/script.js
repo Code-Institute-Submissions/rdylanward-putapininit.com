0a7d8dce02eaf1c233a82b504eadf03147fe2285
rdylanward <r.dylan.ward@gmail.com>
2021-03-28 11:40:12 +0100

    Completed styling of the bookings page. Started the JScript. TBC...

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,21 @@ btnOrange.addEventListener("mouseleave", () => {
 
 
 /**********************************************************************/
+/***                          CURRENT DATE                          ***/
+/**********************************-***********************************/
+
+/* The following Javascript was originally written by W3Schools (https://www.w3schools.com/js/js_dates.asp)
+   and repurposed to aid in the function of the date elements of the bookings form */
+function getDate() {
+    let currentDate = new Date();
+
+    document.getElementById("from-date").innerHTML = currentDate;
+    document.getElementById("to-date").innerHTML = currentDate;
+}
+
+
+
+/**********************************************************************/
 /***                     Initialise Parameters                      ***/
 /**********************************-***********************************/
 
