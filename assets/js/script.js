2a3ffda370cb906637714eb60874d8078694b1cc
rdylanward <r.dylan.ward@gmail.com>
2021-04-01 19:11:39 +0100

    Configured and styled the map.

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,35 @@ function obtainDate() {
     console.log(formattedDate(new Date()));
     /* return formattedDate(new Date()); */
 }
+
+
+/**********************************************************************/
+/***                              MAP                               ***/
+/**********************************-***********************************/
+
+/* The following Javascript was originally written for and provided by the
+   Google Maps Platform Maps JavaScript API Guide and repurposed to aid in
+   the function and design of the map */
+let newMap;
+
+function initMap() {
+     /* Create the new map inside the 'map-window' div element */
+    newMap = new google.maps.Map(document.getElementById("map-window"), {
+        /* Define the center of the map */
+        center: { lat: 52.27775, lng: -7.06749 },
+
+        /* Set the initial zoom level of the map */
+        zoom: 5,
+
+        /* Set the map type */
+        mapTypeId: 'hybrid',
+
+        mapID: '829a7370d84c8575'
+    });
+
+    /* Set a tilt angle of the map when zoomed in using browsers that support the feature */
+    newMap.setTilt(45);
+}
+
+/* Call the map */
+initMap();
